690f0d60be144cc3c9a232f765ba2a2a57abc4e0
yycx0328 <lyh_oracle@hotmail.com>
2017-05-19 15:28:44 +0800

    wx develop

--- app.js
+++ app.js
@@ -44,12 +44,16 @@ app.use(function (req,res,next) {
                 console.log(size);
                 var buff = Buffer.concat(datas, size);
                 var result = buff.toString(); // iconv.decode(buff, "utf8");// 转码； var result = buff.toString();//不需要转编码,直接tostring
-
+                console.log(result);
                 var json = JSON.stringify(result);
+                console.log(json);
                 var date = new Date();
                 var expireSeconds = json.expires_in;
                 date.setTime(date.getTime()+expireSeconds*1000);
-
+                console.log(JSON.stringify({
+                    access_token : json.access_token,
+                    expires_date : date
+                }));
                 // 设置cookies保存access_token及过期时间
                 req.cookies.set('client_credential',JSON.stringify({
                     access_token : json.access_token,
